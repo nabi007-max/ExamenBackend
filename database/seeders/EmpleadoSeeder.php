a3c70d718e648b33f2544b45fbf50ff44ea94b00
nabi007-max <ibanmqi@icloud.com>
2025-10-27 21:29:14 -0400

    Seeder con ≥10 empleados

--- database/seeders/EmpleadoSeeder.php
+++ database/seeders/EmpleadoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Empleado;
 
 class EmpleadoSeeder extends Seeder
 {
@@ -13,6 +14,65 @@ class EmpleadoSeeder extends Seeder
     public function run(): void
     {
         //
-        
+        Empleado::create([
+            'nombre'=> 'Iban',
+            'apellido'=> 'Mamani',
+            'correo'=> 'ibanmqi@gmail.com',
+            'salario'=>7000.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Bryan',
+            'apellido'=> 'Magne',
+            'correo'=> 'mmagne@gmail.com',
+            'salario'=>5000.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Arturo',
+            'apellido'=> 'Valverde',
+            'correo'=> 'avalverde@gmail.com',
+            'salario'=>3500.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Nicho',
+            'apellido'=> 'Efrain',
+            'correo'=> 'nqfrain@gmail.com',
+            'salario'=>4500.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Juan',
+            'apellido'=> 'Roca',
+            'correo'=> 'jrocad@gmail.com',
+            'salario'=>4500.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Ana Maria',
+            'apellido'=> 'Delin  Seliz',
+            'correo'=> 'Amselin@gicloud.com',
+            'salario'=>3400.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Juan',
+            'apellido'=> 'Brito',
+            'correo'=> 'britoj@icloud.com',
+            'salario'=>6700.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Diana Melani',
+            'apellido'=> 'Murrillo Quispe',
+            'correo'=> 'dmelani04@hotmail.com',
+            'salario'=>4000.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Jesus',
+            'apellido'=> 'Cruz Moriz',
+            'correo'=> 'jcruz@yahoo.com',
+            'salario'=>5600.00,
+        ]);
+                Empleado::create([
+            'nombre'=> 'Masiel',
+            'apellido'=> 'Gomez Torrez',
+            'correo'=> 'msgomezi@alicorp.com',
+            'salario'=>4500.00,
+        ]);
     }
 }
